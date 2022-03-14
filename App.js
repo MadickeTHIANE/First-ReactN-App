c82bb5085a33f3eb3937ee0602ad4e273e9d50e7
Madické Thiane <madicke.thiane@gmail.com>
2022-03-14 11:41:20 +0100

    Imports

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import ListMovies from "./assets/movie";
 
 export default function App() {
   return (
